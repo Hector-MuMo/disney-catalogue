a9822b21478e35ee84bedcf868e59b41e1547c89
Hector Muñoz <hectormm.wrk@gmail.com>
2025-08-18 14:16:04 -0600

    fix | edit GenericError component

--- src/components/GenericError.tsx
+++ src/components/GenericError.tsx
@@ -1,10 +1,14 @@
 import { HeartCrack } from 'lucide-react';
 import type { JSX } from 'react';
 
-const GenericError = (): JSX.Element => {
+interface GenericErrorProps {
+  errorMsg: Error;
+}
+
+const GenericError = ({ errorMsg }: GenericErrorProps): JSX.Element => {
   return (
     <div className="h-screen flex justify-center items-center text-amber-50">
-      <p>Cant connect to the server. Try again later.</p>
+      {errorMsg ? <p>{errorMsg.message}</p> : <p>Cant connect to the server. Try again later.</p>}
       <HeartCrack size={35} />
       <p>Sorry</p>
     </div>
